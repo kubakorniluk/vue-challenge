0480629801d65e8163d538c56d1ae1490077feda
Kuba Korniluk <kornilukkuba@gmail.com>
2022-11-23 15:37:40 +0100

    Changed config for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Config from '../views/Config.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/vue-challange/'),
   routes: [
     {
       path: '/',
